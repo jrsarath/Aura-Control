8c4b78c103de1fdf97e340053c06a49a634615d4
jrsarath <jrsarath@outlook.com>
2024-09-20 15:22:47 +0530

    add identification led

--- main/includes/driver.h
+++ main/includes/driver.h
@@ -50,6 +50,10 @@ driver_handle switch_init(int gpio_pin);
  */
 driver_handle driver_button_init();
 
+void device_identifier_cb();
+void device_commission_window_open_cb();
+void device_commission_window_close_cb();
+
 /** 
  * Driver Update
  * This API should be called to update the driver for the attribute being updated.
