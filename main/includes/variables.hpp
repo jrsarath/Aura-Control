d0c6198ec36b01179736eb8280e4f75247ff5aea
Sarath "Delta" Singh <jrsarath@outlook.com>
2025-12-05 00:19:25 +0530

    rebase

--- main/includes/variables.hpp
+++ main/includes/variables.hpp
@@ -1,23 +1,6 @@
 #pragma once
 
-
-#include <inttypes.h>
 #include "sdkconfig.h"
-#include <driver/gpio.h>
 #include "driver.hpp"
 
-#define VARIABLES_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#define MAX_CONFIGURABLE_PLUGS 8
-#define DEFAULT_POWER false
-#define DEBOUNCE_DELAY_MS 1000
-
 extern const plug plugs[MAX_CONFIGURABLE_PLUGS];
-
-#ifdef __cplusplus
-}
-#endif
